d64e7ac1d7b821e3276eae2ae631851039b1a601
Siva Mahadevan <siva@FreeBSD.org>
2026-07-15 17:21:18 -0400

    SIVA WIP: use POSIX tools to create and parse test reports

--- run_hwprobe.sh
+++ run_hwprobe.sh
@@ -1,59 +1,142 @@
 #
 # SPDX-FileCopyrightText: 2026 Shreeney Ajmeri <ajmerishreeney@gmail.com>
 #
-# SPDX-License-Identifier: BSD-2-Clause     
+# SPDX-License-Identifier: BSD-2-Clause
 #
-
-
 
 #!/bin/sh
 set -e
 
-REPO_DIR=$(pwd)
+: ${SU:=su -m root -c}
 
-TMPDIR=$(mktemp -d /tmp/hwprobe.XXXXXX)
-trap 'rm -rf "$TMPDIR"' EXIT INT TERM
+pcigrep() {
+  pattern="$(printf '%s\n' "$@" | paste -sd '|' -)"
+  grep -Eil "^\\s*(class|subclass)\\s*=\\s*(${pattern})" pcidev* || true
+}
 
-echo "Temporary directory: $TMPDIR"
+filter_pcidev_props() {
+  if test $# -gt 0; then
+    grep -Eh -e '^\S*@\S*:' -e '^\s*(vendor|device|class|subclass)\s*=' "$@"
+  else
+    echo NOT DETECTED
+  fi
+}
 
-echo "Checking for required programs..."
-command -v python3 >/dev/null 2>&1 || { echo "Error: python3 not found."; exit 1; }
-command -v hw-probe >/dev/null 2>&1 || { echo "Error: hw-probe not found."; exit 1; }
-echo "All required programs are available."
+score_pcidevs() {
+  if test $# -gt 0; then
+    num_without_drivers="$(grep '^none[0-9][0-9]*@' "$@" | wc -l)"
+    num_devices=$#
+    score="$(echo "scale=1; (${num_devices} - ${num_without_drivers}) * 2 / ${num_devices}" | bc)"
+  else
+    score=0.0
+  fi
 
-echo "Running hardware probe..."
-su -m root -c "hw-probe -all -save $TMPDIR"
+  echo "${score}/2.0"
+}
 
-echo "Extracting hardware dump..."
-for tgz in "$TMPDIR"/*.tgz; do
-    [ -e "$tgz" ] || continue
-    tar -xf "$tgz" -C "$TMPDIR"
-done
+# See https://www.usb.org/defined-class-codes for USB
+# bDeviceClass and bDeviceSubClass codes
 
-echo "Running script..."
-cd "$TMPDIR"
-python3 "$REPO_DIR/main.py" "$TMPDIR"
+realpath="$(realpath "$0")"
+REPO_DIR="$(dirname "${realpath}")"
 
-MAKER=$(kenv smbios.system.product | sed -E -e 's/[^[:alnum:]]+/_/g' -e 's/(^_|_$)//g')
+>&2 echo "Checking for required programs..."
+command -v hw-probe >/dev/null 2>&1 || { >&2 echo "Error: hw-probe not found."; exit 1; }
+>&2 echo "All required programs are available."
+
+MAKER="$(kenv smbios.system.product | sed -E -e 's/[^[:alnum:]]+/_/g' -e 's/(^_|_$)//g')"
 if [ -z "$MAKER" ]; then
-    echo "Error: Could not determine system product."
+    >&2 echo "Error: Could not determine system product."
     exit 1
 fi
-
 TARGET_DIR="$REPO_DIR/test_results/$MAKER"
+
+TMPDIR="$(mktemp -d /tmp/hwprobe.XXXXXX)"
+trap 'cd -; rm -rf "$TMPDIR"' EXIT INT TERM
+cd $TMPDIR
+>&2 echo "Working inside temporary directory: $PWD"
+
+>&2 echo "Running hardware probe..."
+${SU} "hw-probe -all -save $PWD" >&2
+
+>&2 echo "Extracting hardware dump..."
+tar -xf hw.info.tgz
+
+>&2 echo "Creating a laptop testing report..."
 mkdir -p "$TARGET_DIR"
 
-FOUND=0
-for f in "$TMPDIR"/*.txt; do
-    if [ -e "$f" ]; then
-        FOUND=1
-        mv "$f" "$TARGET_DIR/"
-    fi
-done
+csplit -skf pcidev hw.info/logs/pciconf '/^.*@.*:/' '{99}' 2>/dev/null || true
+#csplit -skf usbdev hw.info/logs/usbconfig '/^.*: </' '{99}' 2>/dev/null || true
 
-if [ "$FOUND" -eq 0 ]; then
-    echo "Warning: No .txt files found to move."
-fi
+# graphics
 
-echo "Finished. Thank you for your contribution!"
+graphics_pci_devs="$(pcigrep vga display)"
+networking_pci_devs="$(pcigrep network)"
+audio_pci_devs="$(pcigrep hda multimedia)"
+storage_pci_devs="$(pcigrep 'mass storage' storage)"
+usb_pci_devs="$(pcigrep usb)"
+bluetooth_pci_devs="$(pcigrep bluetooth)"
 
+# Anonymize the kernel version
+kern_version="$(sysctl -n kern.version | head -1)"
+kern_version="${kern_version%%:*}"
+
+cat <<EOF
+=== FreeBSD Hardware Status Info ===
+
+Running: ${kern_version}
+Hardware: ${MAKER}
+CPU: $(sysctl -n hw.model)
+------------------------------------
+
+- Graphics
+$(filter_pcidev_props ${graphics_pci_devs})
+
+  Category Total Score: $(score_pcidevs ${graphics_pci_devs})
+
+--------------------
+
+- Networking
+$(filter_pcidev_props ${networking_pci_devs})
+
+  Category Total Score: $(score_pcidevs ${networking_pci_devs})
+
+--------------------
+
+- Audio
+$(filter_pcidev_props ${audio_pci_devs})
+
+  Category Total Score: $(score_pcidevs ${audio_pci_devs})
+
+--------------------
+
+- Storage
+$(filter_pcidev_props ${storage_pci_devs})
+
+  Category Total Score: $(score_pcidevs ${storage_pci_devs})
+
+--------------------
+
+- USB Ports
+$(filter_pcidev_props ${usb_pci_devs})
+
+  Category Total Score: $(score_pcidevs ${usb_pci_devs})
+
+--------------------
+
+- Bluetooth
+$(filter_pcidev_props ${bluetooth_pci_devs})
+
+  Category Total Score: $(score_pcidevs ${bluetooth_pci_devs})
+
+--------------------
+
+=== FreeBSD Detailed Status Info ==
+
+Currently loaded kernel modules:
+$(kldstat | awk '{ print $5 }' | tail -n+2 | sort)
+
+====================================
+EOF
+
+>&2 echo "Finished. Thank you for your contribution!"
