36061404f1df70e812a52d7b58b3cf53c49ef660
Siva Mahadevan <siva@FreeBSD.org>
2026-08-04 14:33:59 -0400

    make_report: fix incorrectly quoted list arguments
    Additionally:
    * fix rounding error on decimal scores.
    * fix incorrect output redirection with ${SU}

--- make_report.sh
+++ make_report.sh
@@ -14,13 +14,21 @@ count_args() {
 }
 
 pcigrep() {
+  if test $# -eq 0; then
+    >&2 echo "pcigrep() needs at least one pattern"
+    exit 64 # EX_USAGE
+  fi
   pattern="$(printf '%s\n' "$@" | paste -sd '|' -)"
-  grep -Eil "^\\s*(class|subclass)\\s*=\\s*(${pattern})" pcidev* || true
+  grep -Eil "^\\s*(class|subclass)\\s*=\\s*(${pattern})" pcidev* 2>/dev/null || true
 }
 
 usbgrep() {
+  if test $# -eq 0; then
+    >&2 echo "usbgrep() needs at least one pattern"
+    exit 64 # EX_USAGE
+  fi
   pattern="$(printf '%s\n' "$@" | paste -sd '|' -)"
-  grep -Eil "${pattern}" usbdev* || true
+  grep -Eil "${pattern}" usbdev* 2>/dev/null || true
 }
 
 filter_pcidev_props() {
@@ -31,7 +39,6 @@ filter_pcidev_props() {
 # See https://www.usb.org/defined-class-codes for USB
 # bDeviceClass and bDeviceSubClass codes
 filter_usbdev_props() {
-  test $# -eq 0 && exit 0
   for f in "$@"; do
     grep -Eh -e '^\S*: <' -e '^\s*(bDeviceClass|bDeviceSubClass|iManufacturer|iProduct)\s*=' "${f}"
     head -1 "${f}" | sed -E "s/.*<(.*)>.*/  device = '\\1'/"
@@ -39,11 +46,14 @@ filter_usbdev_props() {
 }
 
 pcidevs_without_drivers() {
-  grep '^none[0-9][0-9]*@' "$@"
+  test $# -eq 0 && exit 0
+  grep '^none[0-9][0-9]*@' "$@" | wc -l
 }
 
-usbdevs_with_drivers() {
-  grep -E '^\S*: \S*: ' "$@"
+usbdevs_without_drivers() {
+  test $# -eq 0 && exit 0
+  n="$(grep -E '^\S*: \S*: ' "$@" | wc -l)"
+  echo $(($# - n))
 }
 
 # $1 = num_devices
@@ -52,7 +62,7 @@ score_category() {
   if test "$1" -eq 0; then
     echo 0.0
   else
-    echo "scale=1; 2.0 - 2.0*$2/$1" | bc
+    awk "BEGIN { printf(\"%.1f\\n\", 2.0 - 2.0*$2/$1) }"
   fi
 }
 
@@ -74,42 +84,57 @@ cd "${TMPDIR}"
 
 >&2 echo "Probing hardware..."
 pciconf -lv > pciconf.txt
-${SU} "usbconfig -v" > usbconfig.txt
+${SU} "usbconfig -v > usbconfig.txt"
 
 csplit -skf pcidev pciconf.txt '/^.*@.*:/' '{99}' 2>/dev/null || true
 csplit -skf usbdev usbconfig.txt '/^[^.]*\.[^.]*: </' '{99}' 2>/dev/null || true
 
 graphics_pci_devs="$(pcigrep vga display)"
-num_graphics="$(count_args "${graphics_pci_devs}")"
-num_graphics_without_drivers="$(pcidevs_without_drivers "${graphics_pci_devs}" | wc -l)"
+graphics_pci_dev_props="$(filter_pcidev_props ${graphics_pci_devs})"
+num_graphics="$(count_args ${graphics_pci_devs})"
+num_graphics_without_drivers="$(pcidevs_without_drivers ${graphics_pci_devs})"
+graphics_score="$(score_category "${num_graphics}" "${num_graphics_without_drivers}")"
 
 networking_pci_devs="$(pcigrep network)"
-num_networking="$(count_args "${networking_pci_devs}")"
-num_networking_without_drivers="$(pcidevs_without_drivers "${networking_pci_devs}" | wc -l)"
+networking_pci_dev_props="$(filter_pcidev_props ${networking_pci_devs})"
+num_networking="$(count_args ${networking_pci_devs})"
+num_networking_without_drivers="$(pcidevs_without_drivers ${networking_pci_devs})"
+networking_score="$(score_category "${num_networking}" "${num_networking_without_drivers}")"
 
 audio_pci_devs="$(pcigrep hda multimedia)"
-num_audio="$(count_args "${audio_pci_devs}")"
-num_audio_without_drivers="$(pcidevs_without_drivers "${audio_pci_devs}" | wc -l)"
+audio_pci_dev_props="$(filter_pcidev_props ${audio_pci_devs})"
+num_audio="$(count_args ${audio_pci_devs})"
+num_audio_without_drivers="$(pcidevs_without_drivers ${audio_pci_devs})"
+audio_score="$(score_category "${num_audio}" "${num_audio_without_drivers}")"
 
 storage_pci_devs="$(pcigrep 'mass storage' storage)"
-num_storage="$(count_args "${storage_pci_devs}")"
-num_storage_without_drivers="$(pcidevs_without_drivers "${storage_pci_devs}" | wc -l)"
+storage_pci_dev_props="$(filter_pcidev_props ${storage_pci_devs})"
+num_storage="$(count_args ${storage_pci_devs})"
+num_storage_without_drivers="$(pcidevs_without_drivers ${storage_pci_devs})"
+storage_score="$(score_category "${num_storage}" "${num_storage_without_drivers}")"
 
 usbports_pci_devs="$(pcigrep usb)"
-num_usbports="$(count_args "${usbports_pci_devs}")"
-num_usbports_without_drivers="$(pcidevs_without_drivers "${usbports_pci_devs}" | wc -l)"
+usbports_pci_dev_props="$(filter_pcidev_props ${usbports_pci_devs})"
+num_usbports="$(count_args ${usbports_pci_devs})"
+num_usbports_without_drivers="$(pcidevs_without_drivers ${usbports_pci_devs})"
+usbports_score="$(score_category "${num_usbports}" "${num_usbports_without_drivers}")"
 
 # Bluetooth devices are also likely found as USB devices
 bluetooth_pci_devs="$(pcigrep bluetooth)"
+bluetooth_pci_dev_props="$(filter_pcidev_props ${bluetooth_pci_devs})"
 bluetooth_usb_devs="$(usbgrep bluetooth)"
-num_bluetooth="$(count_args "${bluetooth_pci_devs}" "${bluetooth_usb_devs}")"
-num_bluetooth_pci_without_drivers="$(pcidevs_without_drivers "${bluetooth_pci_devs}" | wc -l)"
-num_bluetooth_usb_with_drivers="$(usbdevs_with_drivers "${bluetooth_usb_devs}" | wc -l)"
-num_bluetooth_without_drivers=$((num_bluetooth_pci_without_drivers + num_bluetooth - num_bluetooth_usb_with_drivers))
+bluetooth_usb_dev_props="$(filter_usbdev_props ${bluetooth_usb_devs})"
+num_bluetooth="$(count_args ${bluetooth_pci_devs} ${bluetooth_usb_devs})"
+num_bluetooth_pci_without_drivers="$(pcidevs_without_drivers ${bluetooth_pci_devs})"
+num_bluetooth_usb_without_drivers="$(usbdevs_without_drivers ${bluetooth_usb_devs})"
+num_bluetooth_without_drivers=$((num_bluetooth_pci_without_drivers + num_bluetooth_usb_without_drivers))
+bluetooth_score="$(score_category "${num_bluetooth}" "${num_bluetooth_without_drivers}")"
 
 # Anonymize the kernel version
 kern_version="$(sysctl -n kern.version | head -1)"
 kern_version="${kern_version%%:*}"
+kern_modules="$(kldstat | awk '{ print $5 }' | tail -n+2 | sort)"
+hw_model="$(sysctl -n hw.model)"
 
 >&2 echo "Creating a laptop testing report..."
 mkdir -p "${TARGET_DIR}"
@@ -117,58 +142,58 @@ mkdir -p "${TARGET_DIR}"
 cat > "${TARGET_DIR}/probe_$(date '+%F_%H-%M-%S').txt" <<EOF
 === FreeBSD Hardware Status Info ===
 
-Running: "${kern_version}"
-Hardware: "${MAKER}"
-CPU: $(sysctl -n hw.model)
+Running: ${kern_version}
+Hardware: ${MAKER}
+CPU: ${hw_model}
 ------------------------------------
 
 - Graphics
-$(filter_pcidev_props "${graphics_pci_devs}")
+${graphics_pci_dev_props}
 
-  Category Total Score: $(score_category "${num_graphics}" "${num_graphics_without_drivers}")/2.0
+  Category Total Score: ${graphics_score}/2.0
 
 --------------------
 
 - Networking
-$(filter_pcidev_props "${networking_pci_devs}")
+${networking_pci_dev_props}
 
-  Category Total Score: $(score_category "${num_networking}" "${num_networking_without_drivers}")/2.0
+  Category Total Score: ${networking_score}/2.0
 
 --------------------
 
 - Audio
-$(filter_pcidev_props "${audio_pci_devs}")
+${audio_pci_dev_props}
 
-  Category Total Score: $(score_category "${num_audio}" "${num_audio_without_drivers}")/2.0
+  Category Total Score: ${audio_score}/2.0
 
 --------------------
 
 - Storage
-$(filter_pcidev_props "${storage_pci_devs}")
+${storage_pci_dev_props}
 
-  Category Total Score: $(score_category "${num_storage}" "${num_storage_without_drivers}")/2.0
+  Category Total Score: ${storage_score}/2.0
 
 --------------------
 
 - USB Ports
-$(filter_pcidev_props "${usbports_pci_devs}")
+${usbports_pci_dev_props}
 
-  Category Total Score: $(score_category "${num_usbports}" "${num_usbports_without_drivers}")/2.0
+  Category Total Score: ${usbports_score}/2.0
 
 --------------------
 
 - Bluetooth
-$(filter_pcidev_props "${bluetooth_pci_devs}")
-$(filter_usbdev_props "${bluetooth_usb_devs}")
+${bluetooth_pci_dev_props}
+${bluetooth_usb_dev_props}
 
-  Category Total Score: $(score_category "${num_bluetooth}" "${num_bluetooth_without_drivers}")/2.0
+  Category Total Score: ${bluetooth_score}/2.0
 
 --------------------
 
 === FreeBSD Detailed Status Info ==
 
 Currently loaded kernel modules:
-$(kldstat | awk '{ print $5 }' | tail -n+2 | sort)
+${kern_modules}
 
 ====================================
 EOF
